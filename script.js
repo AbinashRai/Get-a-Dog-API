960805f1e3904b3d0a5b09a20ea068fa9e4c3493
Abinash Rai <abinashbantawa1905@gmail.com>
2023-05-26 15:59:41 +0545

    learning API

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const dogImageDiv = document.getElementById('dogImage')
+
+fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => response.json())
+  .then(json => {
+    console.log(json.message)
+       dogImageDiv.innerHTML = `<img src='${json.message}' height=300 width=300/>`
+  })
